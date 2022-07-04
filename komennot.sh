ac71fa04f6a646134b826f8a13ccac0bc6afc3c7
Piia <piia.aro@awacademy.training>
2022-07-04 22:13:47 +0300

    lisätty securitygroupit sekä loadbalancer listener ja targetgroup

--- komennot.sh
+++ komennot.sh
@@ -4,6 +4,10 @@ aws cloudformation deploy --template-file index.yaml --stack-name piian-stack
 
  aws cloudformation deploy --template-file index.yaml --stack-name PiianTestiInstance
 
- aws cloudformation validate-template --template-body file://index.yaml
+ 
 
 latest amiId: /aws/service/ami-amazon-linux-latest/amzn-ami-hvm-x86_64-gp2
+
+
+
+
