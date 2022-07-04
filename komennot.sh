e6ccaf864b02b128562333e9d3b20cbc0069bd05
Piia <piia.aro@awacademy.training>
2022-07-04 13:33:21 +0300

    luotu ec2 PiianTestiInstance

--- komennot.sh
+++ komennot.sh
@@ -1,3 +1,9 @@
 AWS cloudformation create-stack --stack-name piian-stack --template-body file://index.yaml
 
 aws cloudformation deploy --template-file index.yaml --stack-name piian-stack 
+
+ aws cloudformation deploy --template-file index.yaml --stack-name PiianTestiInstance
+
+ aws cloudformation validate-template --template-body file://index.yaml
+
+latest amiId: /aws/service/ami-amazon-linux-latest/amzn-ami-hvm-x86_64-gp2
